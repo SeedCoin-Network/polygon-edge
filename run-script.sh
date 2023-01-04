ece199129c7bb6794444194c0eec2ae28eb40dfc
Renat Gafarov <renatgafarov@icloud.com>
2023-01-05 01:44:27 +0200

    added run script

---
+++ run-script.sh
@@ -0,0 +1,1 @@
+seedcoin-network-exec server --data-dir ./data-dir --chain genesis.json --grpc-address node.seedcoin.network:10000 --libp2p node.seedcoin.network:10002 --jsonrpc node.seedcoin.network:10004 --seal
